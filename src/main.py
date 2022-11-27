76d2c24373d8f1039be554c9a27bcfd0456de617
velzevelt <velzcontact@yandex.ru>
2022-11-27 21:47:52 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -1,11 +1,63 @@
-import tkinter
-import customtkinter
+import matplotlib
+import tkinter as tk
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+matplotlib.use('TkAgg')
 
-root = customtkinter.CTk()
+ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue", "sweetkind"
 
-root.title("Test App")
 
-root.mainloop()
+
+from matplotlib.figure import Figure
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk
+)
+
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Test App')
+
+        # prepare data
+        data = {
+            'Python': 11.27,
+            'C': 11.16,
+            'Java': 10.46,
+            'C++': 7.5,
+            'C#': 5.26
+        }
+        languages = data.keys()
+        popularity = data.values()
+
+        # create a figure
+        figure = Figure(figsize=(6, 4), dpi=100)
+
+        # create FigureCanvasTkAgg object
+        figure_canvas = FigureCanvasTkAgg(figure, self)
+
+        # create the toolbar
+        # NavigationToolbar2Tk(figure_canvas, self)
+
+        # create axes
+        axes = figure.add_subplot()
+
+        # create the barchart
+        axes.bar(languages, popularity)
+        axes.set_title('Top 5 Programming Languages')
+        axes.set_ylabel('Popularity')
+
+        figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1, padx=10, pady=10)
+
+        button_1 = ctk.CTkButton(text="Test Button")
+        button_1.pack(side=tk.BOTTOM)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
